4699c6465e08e3b3ebd2d12856b344c6f397dfcf
Somrat <58769763+tfsomrat@users.noreply.github.com>
2024-05-05 15:51:53 +0600

    remove purgcss as tailwind by default did purge, and update packages and modules

--- exampleSite/postcss.config.js
+++ exampleSite/postcss.config.js
@@ -1,45 +1,6 @@
-const purgecss = {
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const elements = JSON.parse(content).htmlElements;
-    return [
-      ...(elements.tags || []),
-      ...(elements.classes || []),
-      ...(elements.ids || []),
-    ];
-  },
-  safelist: [
-    /^swiper-/,
-    /^lb-/,
-    /^gl/,
-    /^go/,
-    /^gc/,
-    /^gs/,
-    /^gi/,
-    /^gz/,
-    /^gprev/,
-    /^gnext/,
-    /^desc/,
-    /^zoom/,
-    /^search/,
-    /^:is/,
-    /dark/,
-    /show/,
-    /dragging/,
-    /fullscreen/,
-    /loaded/,
-    /visible/,
-    /current/,
-    /active/,
-    /mark/,
-  ],
-};
-
 module.exports = {
   plugins: {
     tailwindcss: {},
-    "@fullhuman/postcss-purgecss":
-      process.env.HUGO_ENVIRONMENT === "production" ? purgecss : false,
-    autoprefixer: process.env.HUGO_ENVIRONMENT === "production" ? {} : false,
+    autoprefixer: {},
   },
 };
