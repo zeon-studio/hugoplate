f46363e24cc43925e49c10698c2112b4fef16945
DzenVm <prosumerent@gmail.com>
2025-05-04 18:20:50 +0000

    1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const twTheme = require('./tailwind-plugin/tw-theme.js')
+const twGrid = require('./tailwind-plugin/tw-bs-grid.js')
+
+module.exports = {
+  content: [
+    './layouts/**/*.{html,js}',
+    './content/**/*.{md,html}',
+    './themes/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    twTheme,
+    twGrid,
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
