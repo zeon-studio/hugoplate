4059fc2328390743415e0f9997aad90ebfd4be28
Somrat <58769763+tfsomrat@users.noreply.github.com>
2025-10-29 14:11:17 +0600

    update tailwind plugins

--- exampleSite/tailwind-plugin/tw-bs-grid.js
+++ exampleSite/tailwind-plugin/tw-bs-grid.js
@@ -1,109 +1,122 @@
-import plugin from "tailwindcss/plugin";
+const plugin = require("tailwindcss/plugin");
 
-(function () {
-  "use strict";
-  module.exports = plugin.withOptions(() => {
-    return ({ addComponents }) => {
-      const gridColumns = 12;
-      const gridGutterWidth = "1.5rem";
-      const gridGutters = {
-        0: "0",
-        1: "0.25rem",
-        2: "0.5rem",
-        3: "1rem",
-        4: "1.5rem",
-        5: "3rem",
-      };
-      const respectImportant = true;
-      const columns = Array.from({ length: gridColumns }, (_, i) => i + 1);
-      const rowColsSteps = columns.slice(0, Math.floor(gridColumns / 2));
+module.exports = plugin.withOptions(() => {
+  return ({ addComponents }) => {
+    const gridColumns = 12;
+    const gridGutterWidth = "1.5rem";
+    const gridGutters = {
+      0: "0",
+      1: "0.25rem",
+      2: "0.5rem",
+      3: "1rem",
+      4: "1.5rem",
+      5: "3rem",
+    };
+    const respectImportant = true;
+    const columns = Array.from({ length: gridColumns }, (_, i) => i + 1);
+    const rowColsSteps = columns.slice(0, Math.floor(gridColumns / 2));
 
-      // row
-      addComponents(
-        {
-          ".row": {
-            "--bs-gutter-x": gridGutterWidth,
-            "--bs-gutter-y": 0,
-            display: "flex",
-            flexWrap: "wrap",
-            marginTop: "calc(var(--bs-gutter-y) * -1)",
-            marginRight: "calc(var(--bs-gutter-x) / -2)",
-            marginLeft: "calc(var(--bs-gutter-x) / -2)",
-            "& > *": {
-              boxSizing: "border-box",
-              flexShrink: 0,
-              width: "100%",
-              maxWidth: "100%",
-              paddingRight: "calc(var(--bs-gutter-x) / 2)",
-              paddingLeft: "calc(var(--bs-gutter-x) / 2)",
-              marginTop: "var(--bs-gutter-y)",
-            },
+    // Row
+    addComponents(
+      {
+        ".row": {
+          "--bs-gutter-x": gridGutterWidth,
+          "--bs-gutter-y": "0",
+          display: "flex",
+          flexWrap: "wrap",
+          marginTop: "calc(var(--bs-gutter-y) * -1)",
+          marginRight: "calc(var(--bs-gutter-x) / -2)",
+          marginLeft: "calc(var(--bs-gutter-x) / -2)",
+          "> *": {
+            boxSizing: "border-box",
+            flexShrink: "0",
+            width: "100%",
+            maxWidth: "100%",
+            paddingRight: "calc(var(--bs-gutter-x) / 2)",
+            paddingLeft: "calc(var(--bs-gutter-x) / 2)",
+            marginTop: "var(--bs-gutter-y)",
           },
         },
-        { respectImportant },
-      );
+      },
+      { respectImportant },
+    );
 
-      // columns
-      addComponents(
-        [
-          {
-            ".col": { flex: "1 0 0%" },
-            ".row-cols-auto": { "& > *": { flex: "0 0 auto", width: "auto" } },
+    // Columns + helper row-cols
+    addComponents([
+      {
+        ".col": {
+          flex: "1 0 0%",
+          width: "initial",
+          display: "initial",
+        },
+        ".row-cols-auto": {
+          "> *": {
+            flex: "0 0 auto",
+            width: "auto",
           },
-          ...rowColsSteps.map((num) => ({
-            [`.row-cols-${num}`]: {
-              "& > *": { flex: "0 0 auto", width: `${100 / num}%` },
-            },
-          })),
-          { ".col-auto": { flex: "0 0 auto", width: "auto" } },
-          ...columns.map((num) => ({
-            [`.col-${num}`]: {
-              flex: "0 0 auto",
-              width: `${(100 / gridColumns) * num}%`,
-            },
-          })),
-        ],
-        { respectImportant },
-      );
+        },
+      },
+      ...rowColsSteps.map((rowCol) => ({
+        [`.row-cols-${rowCol}`]: {
+          "> *": {
+            flex: "0 0 auto",
+            width: `${100 / rowCol}%`,
+            display: "initial",
+          },
+        },
+      })),
+      {
+        ".col-auto": {
+          flex: "0 0 auto",
+          width: "auto",
+        },
+      },
+      // explicit sized columns
+      ...columns.map((size) => ({
+        [`.col-${size}`]: {
+          flex: "0 0 auto",
+          width: `${(100 / gridColumns) * size}%`,
+        },
+      })),
+    ]);
 
-      // offset
-      addComponents(
-        [0, ...columns.slice(0, -1)].map((num) => ({
-          [`.offset-${num}`]: { marginLeft: `${(100 / gridColumns) * num}%` },
+    // Offsets
+    addComponents(
+      [0, ...columns.slice(0, -1)].map((num) => ({
+        [`.offset-${num}`]: { marginLeft: `${(100 / gridColumns) * num}%` },
+      })),
+      { respectImportant },
+    );
+
+    // Gutters
+    if (Object.keys(gridGutters).length) {
+      const gutterComponents = Object.entries(gridGutters).reduce(
+        (acc, [key, value]) => {
+          acc[`.g-${key}`] = {
+            "--bs-gutter-x": value,
+            "--bs-gutter-y": value,
+          };
+          acc[`.gx-${key}`] = { "--bs-gutter-x": value };
+          acc[`.gy-${key}`] = { "--bs-gutter-y": value };
+          return acc;
+        },
+        {},
+      );
+      addComponents(gutterComponents, { respectImportant });
+    }
+
+    // Ordering helpers
+    addComponents(
+      [
+        {
+          ".order-first": { order: "-1" },
+          ".order-last": { order: String(gridColumns + 1) },
+        },
+        ...[0, ...columns].map((num) => ({
+          [`.order-${num}`]: { order: String(num) },
         })),
-        { respectImportant },
-      );
-
-      // gutters
-      if (Object.keys(gridGutters).length) {
-        const gutterComponents = Object.entries(gridGutters).reduce(
-          (acc, [key, value]) => {
-            acc[`.g-${key}`] = {
-              "--bs-gutter-x": value,
-              "--bs-gutter-y": value,
-            };
-            acc[`.gx-${key}`] = { "--bs-gutter-x": value };
-            acc[`.gy-${key}`] = { "--bs-gutter-y": value };
-            return acc;
-          },
-          {},
-        );
-        addComponents(gutterComponents, { respectImportant });
-      }
-
-      // order
-      addComponents(
-        [
-          {
-            ".order-first": { order: "-1" },
-            ".order-last": { order: gridColumns + 1 },
-          },
-          ...[0, ...columns].map((num) => ({
-            [`.order-${num}`]: { order: String(num) },
-          })),
-        ],
-        { respectImportant },
-      );
-    };
-  });
-})();
+      ],
+      { respectImportant },
+    );
+  };
+});
